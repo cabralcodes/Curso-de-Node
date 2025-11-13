9ea249618fa74981b5a5ead4110d78ba655017ba
cabralcodes <matheusaugustodev.web@gmail.com>
2025-11-13 15:43:01 -0300

    aula 07 incompleta

---
+++ Aula 07/index.js
@@ -0,0 +1,30 @@
+// lembrar do package, e se tiver muita dúvida rever o curso de node aula 07.
+
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const __dirname = path.resolve();
+
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname, "../html/index.html"));
+});
+
+app.get("/sobre", function(req,res){
+    res.send("minha página sobre")
+
+});
+
+app.get("/blog", function(req,res){
+    res.send("Bem vindo ao meu Blog!")
+});
+
+app.get('/ola/:nome/:cargo', function(req, res){
+    res.send("<h1>Ola " + req.params.nome + "</h1>"+"<h2>Seu cargo é: " + req.params.cargo + "</h2>");
+  
+})
+
+app.listen(49823, function(){
+    console.log("Servidor rodando na URL http://localhost:49823/");
+});
+
